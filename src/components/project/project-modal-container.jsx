ec350bad838be50fc919a34b258dd08d601cf984
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-08 12:49:26 +0100

    Added project modal

---
+++ src/components/project/project-modal-container.jsx
@@ -0,0 +1,39 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+
+export const ProjectModalContainer = (props) => (
+  <Transition appear show={props.show} as={Fragment}>
+    <Dialog as="div" className="relative z-10" onClose={props.closeModal}>
+      {/* Backdrop */}
+      <Transition.Child
+        as={Fragment}
+        enter="ease-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="fixed inset-0 bg-black bg-opacity-70" />
+      </Transition.Child>
+
+      <div className="fixed inset-0 overflow-y-auto">
+        <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Dialog.Panel className="w-full max-w-3xl transform overflow-hidden rounded-2xl bg-stone-900 p-6 text-left align-middle shadow-xl transition-all">
+              {props.children}
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </div>
+    </Dialog>
+  </Transition>
+);
---
+++ src/components/project/project-modal.jsx
@@ -0,0 +1,116 @@
+import Lightbox from "react-spring-lightbox";
+import clsx from "clsx";
+import { Dialog } from "@headlessui/react";
+import { FaGithub, FaTimes } from "react-icons/fa";
+import { Gallery } from "react-grid-gallery";
+import { useState } from "react";
+
+import { ProjectModalContainer } from "./project-modal-container";
+import { ProjectTechnologies } from "./project-technologies";
+
+export const ProjectModal = ({ project, closeModal }) => {
+  const [showLightbox, setShowLightbox] = useState(false);
+
+  const [currentImageIndex, setCurrentIndex] = useState(0);
+  const handleImageClick = (i) => {
+    setCurrentIndex(i);
+    setShowLightbox(true);
+  };
+
+  const gotoPrevious = () =>
+    currentImageIndex > 0 && setCurrentIndex(currentImageIndex - 1);
+
+  const gotoNext = () =>
+    currentImageIndex + 1 < project.images.length &&
+    setCurrentIndex(currentImageIndex + 1);
+
+  const handleLightboxClose = () => setShowLightbox(false);
+
+  return (
+    <>
+      <ProjectModalContainer show={!showLightbox} closeModal={closeModal}>
+        <div className="space-y-4">
+          {/* Header */}
+          <div className="flex gap-2">
+            <img
+              src={project.icon}
+              alt={project.title}
+              className="w-10 h-10 rounded-lg flex-none"
+            />
+
+            {/* Details */}
+            <div className="flex-1 flex flex-col gap-1">
+              <Dialog.Title as="h3" className="text-lg font-fredoka-one">
+                {project.title}
+              </Dialog.Title>
+
+              <ProjectTechnologies project={project} />
+
+              <Dialog.Description className="text-sm">
+                {project.description}
+              </Dialog.Description>
+
+              <div className="flex gap-2 mt-2">
+                {/* Visit link */}
+                <a
+                  target="_blank"
+                  href={project.link}
+                  className={clsx(
+                    "rounded-lg bg-green-500 text-green-900",
+                    "font-bold px-4 py-1 outline-0 rounded-lg",
+                    "hover:ring hover:ring-green-700",
+                    "focus:ring focus:ring-green-700",
+                    "border border-green-500"
+                  )}
+                >
+                  Visit Page
+                </a>
+
+                {/* Repo */}
+                {project.repo ? (
+                  <a
+                    target="_blank"
+                    href={project.repo}
+                    className={clsx(
+                      "font-bold px-4 py-1 outline-0 rounded-lg",
+                      "hover:ring hover:ring-green-700",
+                      "focus:ring focus:ring-green-700",
+                      "border border-green-500"
+                    )}
+                  >
+                    <FaGithub className="w-6 h-6" />
+                  </a>
+                ) : null}
+              </div>
+            </div>
+
+            {/* Close button */}
+            <button
+              onClick={closeModal}
+              className={clsx(
+                "self-start outline-0 rounded-lg focus:ring focus:ring-green-700"
+              )}
+            >
+              <FaTimes className="w-6 h-6" />
+            </button>
+          </div>
+          <Gallery
+            enableImageSelection={false}
+            images={project.images}
+            onClick={handleImageClick}
+          />
+        </div>
+      </ProjectModalContainer>
+
+      <Lightbox
+        isOpen={showLightbox}
+        onClose={handleLightboxClose}
+        images={project.images}
+        onPrev={gotoPrevious}
+        onNext={gotoNext}
+        currentIndex={currentImageIndex}
+        className="bg-black bg-opacity-70"
+      />
+    </>
+  );
+};
