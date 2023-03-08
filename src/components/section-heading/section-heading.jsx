0b87c63cd7118b74eff2f7fe8d3b8964c6b83872
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-08 07:55:57 +0100

    Added section heading

---
+++ src/components/section-heading/section-heading.jsx
@@ -0,0 +1,7 @@
+import { forwardRef } from "react";
+
+export const SectionHeading = forwardRef((props, ref) => (
+  <h3 ref={ref} className="text-2xl text-green-400 font-fredoka-one p-2">
+    {props.children}
+  </h3>
+));
