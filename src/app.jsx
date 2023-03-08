8adacbb611ec33955ebafacecd53469c1d27ef74
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-08 08:00:51 +0100

    Added sections: About, Contact and Resume

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,23 @@
+import { About } from "./sections/about/about";
+import { Contact } from "./sections/contact/contact";
 import { Footer } from "./sections/footer/footer";
 import { Hero } from "./sections/hero/hero";
+import { MainArea } from "./partials/main-area/main-area";
+import { Resume } from "./sections/resume/resume";
+import { SectionGroup } from "./components/section-group/section-group";
 
 export const App = () => (
   <>
     <Hero />
+    <MainArea>
+      <SectionGroup>
+        <About />
+      </SectionGroup>
+      <SectionGroup>
+        <Contact />
+        <Resume />
+      </SectionGroup>
+    </MainArea>
     <Footer />
   </>
 );
