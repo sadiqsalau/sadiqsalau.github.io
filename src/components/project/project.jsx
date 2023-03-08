2f9a3e73e611307fbb9d87c3f0f60711a9910f53
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-08 10:28:01 +0100

    Extracted project component

---
+++ src/components/project/project.jsx
@@ -0,0 +1,43 @@
+import clsx from "clsx";
+
+export const Project = ({ project }) => (
+  <a
+    href={project.link}
+    target="_blank"
+    className={clsx(
+      "border border-green-500",
+      "rounded-lg p-2.5",
+      "flex flex-col gap-2",
+      "shadow-[5px_5px_0px_-2px_theme(colors.green.700)]",
+      "hover:bg-stone-800"
+    )}
+  >
+    <div className="flex gap-2">
+      {/* Icon */}
+      <img
+        src={project.icon}
+        alt={project.title}
+        className="w-10 h-10 rounded-lg flex-none"
+      />
+      {/* Details */}
+      <div className="flex-1 flex flex-col gap-1">
+        <h4 className="font-fredoka-one">{project.title}</h4>
+
+        {/* Technologies */}
+        <ul className="flex gap-2 flex-wrap">
+          {project.technologies.map((Icon, i) => (
+            <li key={i}>
+              <Icon className="text-stone-400" />
+            </li>
+          ))}
+        </ul>
+
+        <p className="text-sm">{project.description}</p>
+
+        <div className="bg-stone-700 text-sm px-2 rounded-full self-start">
+          {project.position}
+        </div>
+      </div>
+    </div>
+  </a>
+);
--- src/sections/projects/projects.jsx
+++ src/sections/projects/projects.jsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import { SectionHeading } from "@/components/section-heading/section-heading";
 
 import { projectsList } from "./projects-list";
+import { Project } from "@/components/project/project";
 
 export const Projects = () => (
   <div className="flex flex-col gap-2">
@@ -13,46 +14,7 @@ export const Projects = () => (
       )}
     >
       {projectsList.map((project, i) => (
-        <a
-          key={i}
-          href={project.link}
-          target="_blank"
-          className={clsx(
-            "border border-green-500",
-            "rounded-lg p-2.5",
-            "flex flex-col gap-2",
-            "shadow-[5px_5px_0px_-2px_theme(colors.green.700)]",
-            "hover:bg-stone-800"
-          )}
-        >
-          <div className="flex gap-2">
-            {/* Icon */}
-            <img
-              src={project.icon}
-              alt={project.title}
-              className="w-10 h-10 rounded-lg flex-none"
-            />
-            {/* Details */}
-            <div className="flex-1 flex flex-col gap-1">
-              <h4 className="font-fredoka-one">{project.title}</h4>
-
-              {/* Technologies */}
-              <ul className="flex gap-2 flex-wrap">
-                {project.technologies.map((Icon, i) => (
-                  <li key={i}>
-                    <Icon className="text-stone-400" />
-                  </li>
-                ))}
-              </ul>
-
-              <p className="text-sm">{project.description}</p>
-
-              <div className="bg-stone-700 text-sm px-2 rounded-full self-start">
-                {project.position}
-              </div>
-            </div>
-          </div>
-        </a>
+        <Project key={i} project={project} />
       ))}
     </div>
   </div>
