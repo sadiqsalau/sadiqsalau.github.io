a53a6c374fc4b7c40429fb77dca7052351c4b8f6
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-08 06:49:40 +0100

    Aliased src folder

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
   plugins: [react()],
-})
+});
