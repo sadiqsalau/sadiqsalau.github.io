7ce70ef44df1d2e1e47d9172ea4fa11fdb32c13a
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-08 06:56:25 +0100

    Overrides green shades

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          100: "#dcffec",
+          200: "#b9ffd9",
+          300: "#97ffc6",
+          400: "#74ffb3",
+          500: "#51ffa0",
+          600: "#41cc80",
+          700: "#319960",
+          800: "#206640",
+          900: "#103320",
+        },
+      },
+    },
   },
   plugins: [],
 };
