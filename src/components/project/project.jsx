527cc2f1773b1d8721a8bf589911a8a454da5684
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-08 12:50:27 +0100

    Enabled modal toggling Render technologies component

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -1,43 +1,52 @@
 import clsx from "clsx";
+import { useState } from "react";
 
-export const Project = ({ project }) => (
-  <a
-    href={project.link}
-    target="_blank"
-    className={clsx(
-      "border border-green-500",
-      "rounded-lg p-2.5",
-      "flex flex-col gap-2",
-      "shadow-[5px_5px_0px_-2px_theme(colors.green.700)]",
-      "hover:bg-stone-800"
-    )}
-  >
-    <div className="flex gap-2">
-      {/* Icon */}
-      <img
-        src={project.icon}
-        alt={project.title}
-        className="w-10 h-10 rounded-lg flex-none"
-      />
-      {/* Details */}
-      <div className="flex-1 flex flex-col gap-1">
-        <h4 className="font-fredoka-one">{project.title}</h4>
+import { ProjectModal } from "./project-modal";
+import { ProjectTechnologies } from "./project-technologies";
 
-        {/* Technologies */}
-        <ul className="flex gap-2 flex-wrap">
-          {project.technologies.map((Icon, i) => (
-            <li key={i}>
-              <Icon className="text-stone-400" />
-            </li>
-          ))}
-        </ul>
+export const Project = ({ project }) => {
+  const [showModal, setShowModal] = useState();
 
-        <p className="text-sm">{project.description}</p>
+  const handleClick = () => setShowModal(true);
+  const handleModalClose = () => setShowModal(false);
 
-        <div className="bg-stone-700 text-sm px-2 rounded-full self-start">
-          {project.position}
+  return (
+    <>
+      <div
+        onClick={handleClick}
+        className={clsx(
+          "border border-green-500",
+          "rounded-lg p-2.5",
+          "flex flex-col gap-2",
+          "shadow-[5px_5px_0px_-2px_theme(colors.green.700)]",
+          "hover:bg-stone-800"
+        )}
+      >
+        <div className="flex gap-2">
+          {/* Icon */}
+          <img
+            src={project.icon}
+            alt={project.title}
+            className="w-10 h-10 rounded-lg flex-none"
+          />
+          {/* Details */}
+          <div className="flex-1 flex flex-col gap-1">
+            <h4 className="font-fredoka-one">{project.title}</h4>
+
+            {/* Technologies */}
+            <ProjectTechnologies project={project} />
+
+            <p className="text-sm">{project.description}</p>
+
+            <div className="bg-stone-700 text-sm px-2 rounded-full self-start">
+              {project.position}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </a>
-);
+      {showModal ? (
+        <ProjectModal project={project} closeModal={handleModalClose} />
+      ) : null}
+    </>
+  );
+};
