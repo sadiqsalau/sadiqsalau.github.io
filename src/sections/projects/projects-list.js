5031abca4ee45b63d29be97d0dff3cea240bdfd1
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-08 13:54:16 +0100

    Imported project images

--- src/sections/projects/projects-list.js
+++ src/sections/projects/projects-list.js
@@ -1,4 +1,9 @@
 import MochiIcon from "@/assets/images/projects/mochi/icon.png";
+import MochiImage01 from "@/assets/images/projects/mochi/screenshots/01.png";
+import MochiImage02 from "@/assets/images/projects/mochi/screenshots/02.png";
+import MochiImage03 from "@/assets/images/projects/mochi/screenshots/03.png";
+import MochiImage04 from "@/assets/images/projects/mochi/screenshots/04.png";
+import MochiImage05 from "@/assets/images/projects/mochi/screenshots/05.png";
 import PluxScoreIcon from "@/assets/images/projects/pluxscore/icon.png";
 import PluxScoreImage01 from "@/assets/images/projects/pluxscore/screenshots/01.png";
 import PluxScoreImage02 from "@/assets/images/projects/pluxscore/screenshots/02.png";
@@ -39,6 +44,32 @@ export const projectsList = [
     position: "Open-Source",
     technologies: [SiReact, SiJavascript, SiTailwindcss],
     description: "ReactJs Chat Template",
-    images: [],
+    images: [
+      {
+        src: MochiImage01,
+        width: 319,
+        height: 567,
+      },
+      {
+        src: MochiImage02,
+        width: 319,
+        height: 567,
+      },
+      {
+        src: MochiImage03,
+        width: 319,
+        height: 567,
+      },
+      {
+        src: MochiImage04,
+        width: 750,
+        height: 1334,
+      },
+      {
+        src: MochiImage05,
+        width: 1366,
+        height: 656,
+      },
+    ],
   },
 ];
