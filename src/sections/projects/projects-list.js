e2f4d44abbd68b9ee03c6600b1ee1711c77f15fb
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-08 12:51:18 +0100

    Updated projects list with images

--- src/sections/projects/projects-list.js
+++ src/sections/projects/projects-list.js
@@ -1,5 +1,7 @@
 import MochiIcon from "@/assets/images/projects/mochi/logo.svg";
 import PluxScoreIcon from "@/assets/images/projects/pluxscore/icon@128.png";
+import PluxScoreImage01 from "@/assets/images/projects/pluxscore/screenshots/01.png";
+import PluxScoreImage02 from "@/assets/images/projects/pluxscore/screenshots/02.png";
 import {
   SiBootstrap,
   SiJavascript,
@@ -16,13 +18,27 @@ export const projectsList = [
     position: "Full-Stack Developer",
     technologies: [SiReact, SiBootstrap, SiLaravel],
     description: "Social media for sport fans",
+    images: [
+      {
+        src: PluxScoreImage01,
+        width: 319,
+        height: 567,
+      },
+      {
+        src: PluxScoreImage02,
+        width: 319,
+        height: 567,
+      },
+    ],
   },
   {
     title: "Mochi",
-    link: "https://github.com/sadiqsalau/mochi",
+    link: "https://sadiqsalau.github.io/mochi",
+    repo: "https://github.com/sadiqsalau/mochi",
     icon: MochiIcon,
     position: "Open-Source",
     technologies: [SiReact, SiJavascript, SiTailwindcss],
     description: "ReactJs Chat Template",
+    images: [],
   },
 ];
