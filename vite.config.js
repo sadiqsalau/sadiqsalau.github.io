c672e58e9c4b31417c4247741b6846c3b72b733e
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-08 14:44:49 +0100

    Removed PWA plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
-import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import { resolve } from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -9,36 +9,5 @@ export default defineConfig({
       "@": resolve(__dirname, "src"),
     },
   },
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-      scope: "./",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
-      workbox: {
-        globPatterns: ["**/[!_]*"],
-        navigateFallbackAllowlist: [],
-      },
-      manifest: {
-        name: "Sadiq Salau",
-        short_name: "Sadiq Salau",
-        description: "Full-Stack Web Developer",
-        theme_color: "#292524",
-        background_color: "#292524",
-        start_url: "./",
-        icons: [
-          {
-            src: "icon@192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "icon@512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-    react(),
-  ],
+  plugins: [react()],
 });
