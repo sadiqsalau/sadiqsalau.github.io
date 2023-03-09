aaeed6895969d2c8101ec29b7cb1ec1290ec69b8
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-09 09:45:14 +0100

    Show PWAPrompt

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,12 @@
-import { SectionGroup } from "./components/section-group/section-group";
-import { MainArea } from "./partials/main-area/main-area";
 import { About } from "./sections/about/about";
 import { Contact } from "./sections/contact/contact";
 import { Footer } from "./sections/footer/footer";
 import { Hero } from "./sections/hero/hero";
+import { MainArea } from "./partials/main-area/main-area";
+import { PWAPrompt } from "./partials/pwa-prompt/pwa-prompt";
 import { Projects } from "./sections/projects/projects";
 import { Resume } from "./sections/resume/resume";
+import { SectionGroup } from "./components/section-group/section-group";
 
 export const App = () => (
   <>
@@ -21,5 +22,6 @@ export const App = () => (
       </SectionGroup>
     </MainArea>
     <Footer />
+    <PWAPrompt />
   </>
 );
