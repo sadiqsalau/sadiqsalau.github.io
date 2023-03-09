1f006026909811ce2f6f9ee3ec662bbadbad50a3
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-09 07:16:43 +0100

    Set icon purpose to "maskable"

--- vite.config.js
+++ vite.config.js
@@ -42,7 +42,7 @@ export default defineConfig({
             src: "icon@512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable",
+            purpose: "maskable",
           },
         ],
       },
