caea679479531b74339016f801d454262eaaf4cd
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-09 07:06:35 +0100

    Registered maskable icon

--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,12 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           },
+          {
+            src: "icon@512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
         ],
       },
     }),
