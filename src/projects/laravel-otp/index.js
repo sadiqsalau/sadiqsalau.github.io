c0a0da1b61bc1bc81fa6473eddf7455f94d1790e
Sadiq Salau <sadiqsalau888@gmail.com>
2023-03-12 11:50:27 +0100

    Added project: sadiqsalau/laravel-otp

---
+++ src/projects/laravel-otp/index.js
@@ -0,0 +1,15 @@
+import { SiComposer, SiLaravel, SiPhp } from "react-icons/si";
+
+import icon from "./icon.png";
+
+export const project = {
+  icon,
+  title: "sadiqsalau/laravel-otp",
+  link: "https://packagist.org/packages/sadiqsalau/laravel-otp",
+  repo: "https://github.com/sadiqsalau/laravel-otp",
+  tags: ["Open-Source", "Package"],
+  technologies: [SiComposer, SiLaravel, SiPhp],
+  description: "OTP Package for Laravel",
+  fullDescription:
+    "Seamlessly incorporate OTP code generation and verification into your Laravel application using this package.",
+};
