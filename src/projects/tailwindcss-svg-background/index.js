65012d33d961e437b4e505edc68f0ac0e2f22c02
Sadiq Salau <sadiqsalau888@gmail.com>
2023-04-04 12:43:16 +0100

    Added project: tailwind-css-background

---
+++ src/projects/tailwindcss-svg-background/index.js
@@ -0,0 +1,15 @@
+import { SiJavascript, SiTailwindcss } from "react-icons/si";
+
+import icon from "./icon.png?w=80&h=80";
+
+export const project = {
+  icon,
+  title: "tailwindcss-svg-background",
+  link: "https://www.npmjs.com/package/tailwindcss-svg-background",
+  repo: "https://github.com/sadiqsalau/tailwindcss-svg-background",
+  tags: ["Open-Source", "Package"],
+  technologies: [SiJavascript, SiTailwindcss],
+  description: "SVG Background Image Plugin",
+  fullDescription:
+    "TailwindCSS plugin to use SVG as background images in your projects.",
+};
