b4c29cf93f8b3e58d29a80e20faf4658aa8d42cc
Sadiq Salau <sadiqsalau888@gmail.com>
2023-04-26 06:32:13 +0100

    Removed react-query

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { App } from "./app";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./services/query-client";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
