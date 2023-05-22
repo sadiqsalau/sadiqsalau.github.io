c5bacf56f6dc224ed5c8a84c99eedd660faa51ee
Sadiq Salau <sadiqsalau888@gmail.com>
2023-05-22 06:08:55 +0100

    Added polyfills config

---
+++ vite.config.polyfills.js
@@ -0,0 +1,20 @@
+/**
+ * pnpm add -D esbuild-plugins-node-modules-polyfill rollup-plugin-polyfill-node
+ */
+import nodePolyfills from "rollup-plugin-polyfill-node";
+import { defineConfig } from "vite";
+import { nodeModulesPolyfillPlugin } from "esbuild-plugins-node-modules-polyfill";
+
+export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      // Enable esbuild polyfill plugins
+      plugins: [nodeModulesPolyfillPlugin()],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills()],
+    },
+  },
+});
