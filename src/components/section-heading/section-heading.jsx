dbd2029d61e85224363b5f3db7da41ada0a253e1
Sadiq Salau <sadiqsalau888@gmail.com>
2023-07-23 17:22:28 +0100

    Centered sections heading

--- src/components/section-heading/section-heading.jsx
+++ src/components/section-heading/section-heading.jsx
@@ -1,7 +1,7 @@
 import { forwardRef } from "react";
 
 export const SectionHeading = forwardRef((props, ref) => (
-  <h3 ref={ref} className="text-2xl text-green-400 font-fredoka-one p-2">
+  <h3 ref={ref} className="text-2xl text-green-400 font-fredoka-one p-2 text-center">
     {props.children}
   </h3>
 ));
