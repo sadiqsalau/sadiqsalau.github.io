14e807ba9f4ed11c95fc0934cae4ba7490704346
Sadiq Salau <sadiqsalau888@gmail.com>
2023-07-28 17:23:11 +0100

    Set background to transparent

--- pwa-assets.config.js
+++ pwa-assets.config.js
@@ -1,10 +1,23 @@
-import {
-  defineConfig,
-  minimalPreset as preset
-} from '@vite-pwa/assets-generator/config'
-
+import { defineConfig } from "@vite-pwa/assets-generator/config";
 
 export default defineConfig({
-  preset,
-  images: ['public/logo.png']
-})
+  preset: {
+    transparent: {
+      sizes: [64, 192, 512],
+      favicons: [[64, "favicon.ico"]],
+    },
+    maskable: {
+      sizes: [512],
+      resizeOptions: {
+        background: { r: 0, g: 0, b: 0, alpha: 0 },
+      },
+    },
+    apple: {
+      sizes: [180],
+      resizeOptions: {
+        background: { r: 0, g: 0, b: 0, alpha: 0 },
+      },
+    },
+  },
+  images: ["public/logo.png"],
+});
