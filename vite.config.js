dd304493d0b5637da399d5290f425b4f824dfa9a
Sadiq Salau <sadiqsalau888@gmail.com>
2023-07-28 16:58:09 +0100

    Configured Ejs plugin and refactored vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,29 @@
 import react from "@vitejs/plugin-react-swc";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import { imagetools } from "vite-imagetools";
+import { loadEnv } from "vite";
 import { resolve } from "path";
 
-import PWAConfig from "./vite.config.pwa";
-import polyfills from "./vite.config.polyfills";
+import viteConfigPWA from "./vite.config.PWA";
+import viteConfigPolyfills from "./vite.config.polyfills";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  ...polyfills,
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    ...viteConfigPolyfills,
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+      },
     },
-  },
-  plugins: [PWAConfig(), imagetools(), react()],
+    plugins: [
+      react(),
+      imagetools(),
+      ViteEjsPlugin(env),
+      VitePWA(viteConfigPWA(env)),
+    ],
+  };
 });
