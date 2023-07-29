d8622595bbbddfdee01014a4a56adba2489fd17a
Sadiq Salau <sadiqsalau888@gmail.com>
2023-07-29 07:38:50 +0100

    Removed polyfills

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,11 @@ import { loadEnv } from "vite";
 import { resolve } from "path";
 
 import viteConfigPWA from "./vite.config.PWA";
-import viteConfigPolyfills from "./vite.config.polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    ...viteConfigPolyfills,
     resolve: {
       alias: {
         "@": resolve("src"),
