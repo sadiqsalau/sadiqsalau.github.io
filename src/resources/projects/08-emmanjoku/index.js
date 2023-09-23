390aa46ebdb0e839054a05a4b5b75ede5328857f
Sadiq Salau <sadiqsalau888@gmail.com>
2023-09-23 22:25:54 +0100

    Added project: Emma Njoku

---
+++ src/resources/projects/08-emmanjoku/index.js
@@ -0,0 +1,34 @@
+import { SiReact, SiTailwindcss } from "react-icons/si";
+
+import icon from "./project-icon.png?w=80&h=80&format=webp";
+
+export default {
+  icon,
+  title: "Emma Njoku",
+  link: "https://emmanjoku.com",
+  tags: ["Assistant Developer"],
+  technologies: [SiReact, SiTailwindcss],
+  description: "emmanjoku.com",
+  images: Object.values(
+    import.meta.glob("./screenshots/*", {
+      eager: true,
+      query: {
+        metadata: "",
+        format: "webp",
+      },
+    })
+  ),
+  previewImages: Object.values(
+    import.meta.glob("./screenshots/*", {
+      eager: true,
+      query: {
+        metadata: "",
+        format: "webp",
+        withoutEnlargement: "",
+        w: 350,
+      },
+    })
+  ),
+  fullDescription:
+    "Emma Njoku is a consummate and dedicated executive with an exceptional entrepreneurial mindset. ",
+};
