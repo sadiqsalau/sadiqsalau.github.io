dc9a2e18e109b1566f7ff6dd35d5da4620908360
Sadiq Salau <sadiqsalau888@gmail.com>
2023-11-01 07:24:22 +0100

    Added project: Twelvesquares

---
+++ src/resources/projects/09-twelvesquares/index.js
@@ -0,0 +1,36 @@
+import { SiLaravel, SiReact, SiTailwindcss } from "react-icons/si";
+
+import icon from "./project-icon.png?w=80&h=80&format=webp";
+
+export default {
+  icon,
+  title: "Twelvesquares",
+  link: "https://twelvesquares.net",
+  tags: ["Developer"],
+  technologies: [SiReact, SiTailwindcss, SiLaravel],
+  description: "twelvesquares.net",
+  images: Object.values(
+    import.meta.glob("./screenshots/*", {
+      eager: true,
+      import: "default",
+      query: {
+        as: "metadata",
+        format: "webp",
+      },
+    })
+  ),
+  previewImages: Object.values(
+    import.meta.glob("./screenshots/*", {
+      eager: true,
+      import: "default",
+      query: {
+        as: "metadata",
+        format: "webp",
+        withoutEnlargement: "",
+        w: 350,
+      },
+    })
+  ),
+  fullDescription:
+    "Twelvesquares Technologies and Services Limited, holds an apex position around Africa in Providing 360 degrees technology, security services and products for our numerous clients.",
+};
