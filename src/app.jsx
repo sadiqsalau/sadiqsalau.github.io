acd7441acabaa4323564c4446a41bb830376902e
Sadiq Salau <sadiqsalau888@gmail.com>
2023-11-08 10:06:20 +0100

    Removed PWAPrompt

--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,6 @@ import { FAQ } from "./sections/faq/faq";
 import { Footer } from "./sections/footer/footer";
 import { Hero } from "./sections/hero/hero";
 import { MainArea } from "./partials/main-area/main-area";
-import { PWAPrompt } from "./partials/pwa-prompt/pwa-prompt";
 import { ProjectModal } from "./partials/project-modal/project-modal";
 import { Projects } from "./sections/projects/projects";
 import { Resume } from "./sections/resume/resume";
@@ -34,7 +33,6 @@ export const App = () => (
     <Footer />
 
     {/* Partials */}
-    <PWAPrompt />
     <ProjectModal />
   </AppProvider>
 );
